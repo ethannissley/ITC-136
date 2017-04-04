0f74cf03cd5cb833e09adea3d9c8f7f487f45cb7
ethannissley <ethannissley96@gmail.com>
2017-04-04 14:48:27 -0700

    Create lab1.sh

---
+++ lab1.sh
@@ -0,0 +1,7 @@
+# this is a comment
+
+whoami  #(tells me which user I’m logged in as)
+pwd #(stands for ‘print working directory’ and tells me where I am)
+ls #(tells me what files are in the directory)
+cd #(Allows me to change into other directories)
+date  #(What time it is on the machine)
