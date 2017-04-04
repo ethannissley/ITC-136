bc88fa1518611b1b6c2a2cceb8c5773e91af2fda
ethannissley <ethannissley96@gmail.com>
2017-04-04 14:54:04 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,7 +1,7 @@
 # Linux commands for: 
 
 whoami  # Tells me which user I’m logged in as (who)
-pwd # Stands for ‘print working directory’ and tells me where I am (where)
-ls # Tells me what files are in the directory (what)
-cd # Allows me to change into other directories( navigation)
-date  #W hat time it is on the machine (when)
+pwd     # Stands for ‘print working directory’ and tells me where I am (where)
+ls      # Tells me what files are in the directory (what)
+cd      # Allows me to change into other directories( navigation)
+date    # What time it is on the machine (when)
